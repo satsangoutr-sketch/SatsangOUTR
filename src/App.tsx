ec927441b94dae8dd8c9c1b6021d33165624c972
satsangoutr-sketch <satsangoutr@gmail.com>
2026-03-08 10:13:16 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import About from "./pages/About";
 import Events from "./pages/Events";
 
 import NotFound from "./pages/NotFound";
@@ -19,7 +18,6 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/about" element={<About />} />
           <Route path="/events" element={<Events />} />
           
           <Route path="*" element={<NotFound />} />
