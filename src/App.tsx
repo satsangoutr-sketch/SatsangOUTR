91a626942b0aa536a8cbc7f339b32678a08d603b
satsangoutr-sketch <satsangoutr@gmail.com>
2026-03-12 23:13:33 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Events from "./pages/Events";
-import Schedule from "./pages/Schedule";
+
 
 import NotFound from "./pages/NotFound";
 
@@ -20,7 +20,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/events" element={<Events />} />
-          <Route path="/Schedule" element={<Schedule />}/>
+          
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
