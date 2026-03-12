f43be6d6feecbacd632c75b38288a5e882fc2c0c
satsangoutr-sketch <satsangoutr@gmail.com>
2026-03-12 20:44:31 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Events from "./pages/Events";
+import Schedule from "./pages/Schedule";
 
 import NotFound from "./pages/NotFound";
 
