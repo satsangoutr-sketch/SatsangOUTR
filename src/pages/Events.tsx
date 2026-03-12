6bf593b2fb4e320810da193b9f60661129dd0793
satsangoutr-sketch <satsangoutr@gmail.com>
2026-03-12 22:46:39 +0530

    Update Events.tsx

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -13,7 +13,7 @@ const Events = () => {
             initial={{ opacity: 0, y: 30 }}
             animate={{ opacity: 1, y: 0 }}
           >
-            <h1 className="text-4xl font-bold text-orange-400">
+            <h1 className="text-7xl font-bold text-White-400">
               Career Counselling Seminar
             </h1>
 
@@ -28,7 +28,7 @@ const Events = () => {
               href="https://docs.google.com/forms/d/e/1FAIpQLSfHAY_jPzZibZcqvQRCHFgbsoCZAi-n0Ub69L4HrjqUpLQuVQ/viewform"
               target="_blank"
               rel="noopener noreferrer"
-              className="px-8 py-3 bg-orange-500 hover:bg-orange-600 text-white font-semibold rounded-lg transition"
+              className="px-8 py-3 bg-[#0b1e3a]-500 hover:bg-[#0b1e3a]-600 text-white font-semibold rounded-lg transition"
             >
               Register Now
             </a>
@@ -45,7 +45,7 @@ const Events = () => {
 
               {/* SESSION 1 */}
               <div className="flex gap-6">
-                <div className="text-orange-400 font-semibold w-32">
+                <div className="text-[#243447]-400 font-semibold w-32">
                   09:00 – 10:00
                 </div>
 
@@ -62,7 +62,7 @@ const Events = () => {
 
               {/* SESSION 2 */}
               <div className="flex gap-6">
-                <div className="text-orange-400 font-semibold w-32">
+                <div className="text-[#243447]-400 font-semibold w-32">
                   10:00 – 10:15
                 </div>
 
@@ -79,7 +79,7 @@ const Events = () => {
 
               {/* SESSION 3 */}
               <div className="flex gap-6">
-                <div className="text-orange-400 font-semibold w-32">
+                <div className="text-[#243447]-400 font-semibold w-32">
                   10:15 – 10:45
                 </div>
 
@@ -95,9 +95,9 @@ const Events = () => {
                       <span className="font-semibold text-white">
                         Welcome Address:
                       </span>{" "}
-                      Dr. Banabhari Mishra
+                      Dr. Banabihari Mishra
                       <div className="text-gray-400 text-xs">
-                        Advisor of Organising Committee (Senior Consultant CVTS, SUMUM, BBSR)
+                       Senior Consultant CVTS, SUMUM, BBSR (Advisor of Organising Committee)
                       </div>
                     </li>
 
@@ -111,15 +111,7 @@ const Events = () => {
                       </div>
                     </li>
 
-                    <li>
-                      <span className="font-semibold text-white">
-                        Guest Speaker:
-                      </span>{" "}
-                      Mr. Surya Prakash Mahapatra
-                      <div className="text-gray-400 text-xs">
-                        Chairman, SOA
-                      </div>
-                    </li>
+                    
 
                     <li>
                       <span className="font-semibold text-white">
@@ -137,7 +129,7 @@ const Events = () => {
                       </span>{" "}
                       Dr. Jubilee Purkayastha
                       <div className="text-gray-400 text-xs">
-                        Scientist & Joint Director, INMAS DRDO
+                        Scientist & Joint Director, INMAS DRDO ,Ministry of Defence, Govt. of India
                       </div>
                     </li>
 
@@ -147,8 +139,8 @@ const Events = () => {
 
               {/* SESSION 4 */}
               <div className="flex gap-6">
-                <div className="text-orange-400 font-semibold w-32">
-                  10:45 – 11:00
+                <div className="text-[#243447]-400 font-semibold w-32">
+                  10:45 AM – 11:00 AM
                 </div>
 
                 <div className="bg-[#132a4d] p-5 rounded-lg border-r-4 border-orange-500 flex-1">
@@ -157,22 +149,87 @@ const Events = () => {
                   </h3>
 
                   <p className="text-gray-400 text-sm">
-                    Networking & Refreshments
+                   -------------------------------
                   </p>
                 </div>
               </div>
 
               {/* SESSION 5 */}
               <div className="flex gap-6">
-                <div className="text-orange-400 font-semibold w-32">
-                  11:15 – 01:00
-                </div>
-
-                <div className="bg-[#132a4d] p-5 rounded-lg border-r-4 border-orange-500 flex-1">
+                <div className="text-[#243447]-400 font-semibold w-32">
+                  11:00 AM – 01:00 AM
+                </div> 
+                                <div className="bg-[#132a4d] p-5 rounded-lg border-r-4 border-[#243447]-500 flex-1">
                   <h3 className="text-white font-bold text-lg">
                     Career Counselling Session
                   </h3>
+                                   <ul className="space-y-2 text-gray-300 text-sm">
 
+                    <li>
+                    Dr. Jubilee Purkayastha 
+                      <div className="text-gray-400 text-xs">
+                        Scientist  & Joint Director, INMAS DRDO ,Ministry of Defence, Govt. of India
+                      </div>
+                    </li>
+
+                    <li>
+                      
+                       •	Dr. Pravakar Mohanty
+                      <div className="text-gray-400 text-xs">
+                      Scientist ‘E’ & Joint Director (R&D), Department of Science & Technology, Government of India
+                      </div>
+                    </li>
+
+                    <li>
+                      <
+                      Mr. Surya Prakash Mahapatra
+                      <div className="text-gray-400 text-xs">
+                       Global Head Talent Transformation, Wipro Ltd.
+                      </div>
+                    </li>
+
+                    <li>
+                      
+                      •	Mr. Kalinga Keshari Rath 
+                      <div className="text-gray-400 text-xs">
+                        MD, EVOS buildcon Pvt. Ltd.
+                      </div>
+                    </li>
+
+                    <li>
+                      
+                      •	Dr. Sutanu Chakraborty
+
+                      <div className="text-gray-400 text-xs">
+                        Professor, Department of Computer Science & Engineering, Indian Institute of Technology (IIT) Madras
+
+                      </div>
+                    </li>
+                                      <li>
+                      
+                      •	Dr. Lingaraj Sahoo
+
+                      <div className="text-gray-400 text-xs">
+Professor, Dept .of Bioscience and Bioengineering, IIT Guwahati, & Adj.Prof. Jifu University, JAPAN
+                      </div>
+                    </li>
+                                     <li><strong>Interactive Q & A Session </strong> Disucssion with all Speakers
+                
+                    </li> 
+
+                  </ul>
+                </div>
+              </div>
+                                  
+                 <div className="flex gap-6">
+                <div className="text-[#243447]-400 font-semibold w-32">
+                  1:00 PM – 01:45 PM
+                  <h3 className="text-white font-bold text-lg">
+                    LUNCH BREAK
+                  </h3>
+                </div> 
+
+                
                   <p className="text-gray-400 text-sm">
                     Expert guidance with interactive Q&A
                   </p>
